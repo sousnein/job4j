4b18f6dfef2d9e191c96647c719df0add4053c4b
sous <souswork@yandex.ru>
2020-02-12 21:12:44 +0300

    Слово начинается с ...[#226477]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = false;
+        for (int i=0;i<pref.length;i++)
+            if(word[i]==pref[i])result=true;
+        return result;
+    }
+}
