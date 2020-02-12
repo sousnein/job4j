aed34087cdff8973c5bff0c6b4d2a23e127e8f47
sous <souswork@yandex.ru>
2020-02-12 18:43:46 +0300

    JavaDoc. Code convention[#226543]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculator;
+
+/**
+ * Class Класс для вычисления арифметических операций   +     -       *       =
+ * @author sousnein
+ * @since 12.02.2020
+ * @version 1
+ */
+public class Calculator {
+    /**
+     * Main.
+     * @param args-args.
+     */
+
+    public static void main(String[] args) {
+        int one = 1,six=6,four=4,five=5;
+        int two = 2;
+        int onePlusTwo = one + two;
+        int sixDivTwo=six/two;
+        int fiveMinusTwo=five-two;
+        int fourTimeTwo=four*two;
+        System.out.println(onePlusTwo);
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+    }
+}
