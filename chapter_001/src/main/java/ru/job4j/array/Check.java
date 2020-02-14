8a4136b077246b34d40f88553ee661f3d6036b62
sousnein <souswork@yandex.ru>
2020-02-14 21:10:15 +0300

    Массив заполнен true или false[#226478]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -5,8 +5,10 @@ public class Check {
         boolean result = false;
         int i=0;
      for (i=0;i<data.length-1;i++)
-         if(data[i]==data[0])if(data[i+1]==data[0])result=true;
-         else break;
+         if(data[i]==data[0]) {
+             if (data[i + 1] == data[0]) result = true;
+         }else break;
+
 
         return result;
     }
