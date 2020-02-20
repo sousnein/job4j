d35ffa1478dbee09a90313b2835edbb33324be99
sousnein <souswork@yandex.ru>
2020-02-20 10:48:37 +0300

    Выигрышные комбинации в сокобан[#226479]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,8 +4,8 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int j=0;j<board.length;j++) {
-            if (board[row][j]=='X' ) {
-                result = true;
+            if (board[row][j]!='X' ) {
+                result = false;
                 break;
             }
         }
