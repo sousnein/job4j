1befbdfd596562b4be82df44c179d5573588c2ea
sousnein <souswork@yandex.ru>
2020-02-20 10:35:51 +0300

    Моно столбец в матрице.[#226487]

--- chapter_001/src/main/java/ru/job4j/array/MonoVertical.java
+++ chapter_001/src/main/java/ru/job4j/array/MonoVertical.java
@@ -2,12 +2,12 @@ package ru.job4j.array;
 
 public class MonoVertical {
     public static boolean monoVertical(char[][] board, int column) {
-        boolean result = false;
+        boolean result = true;
         for (int i=0;i<board.length;i++) {
-            if (board[i][column]=='X') {
-                result = true;
+            if (board[i][column]!='X') {
+                result = false;
+                break;
             }
-            else result=false;
         }
         return result;
     }
