e0eac5a9f733fda7d81c6d5d4b2c6632c43ef1ae
sousnein <souswork@yandex.ru>
2020-03-03 21:30:48 +0300

    Объединить два массива [#124482]

---
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -0,0 +1,50 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Merge {
+
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+       /* for (int i = 0; i < (left.length + right.length); i++) {
+            if (left[left.length - i ] > right[right.length - i ]) {
+               rsl[i] = right[right.length - i - 1];
+               rsl[i++] = left[left.length - i - 1];
+            } else {
+                rsl[i] = left[left.length - i - 1];
+                rsl[i++] = right[right.length - i - 1];
+            }
+        }*/
+
+        int aI = 0;
+        int bI = 0;
+        int i = 0;
+        while (i < rsl.length) {
+            if (aI == left.length && bI < right.length) {
+                rsl[i] = right[bI++];
+            } else if (bI == right.length && aI < left.length) {
+                rsl[i] = left[aI++];
+            } else if (bI == right.length && aI == left.length) {
+                break;
+            } else if (left[aI] < right[bI]) {
+                rsl[i] = left[aI++];
+            } else if (left[aI] == right[bI]) {
+                rsl[i] = right[bI++];
+                rsl[i + 1] = left[aI];
+            } else if (left[aI] > right[bI]) {
+                rsl[i] = right[bI++];
+            }
+            i++;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
