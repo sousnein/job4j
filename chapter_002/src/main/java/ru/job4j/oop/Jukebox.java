30949b9086dc1162549578619fe8b23bca4c2114
sousnein <souswork@yandex.ru>
2020-03-10 21:18:48 +0300

    Вызов метода с аргументами.[#226637]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        if (position == 1){
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2){
+            System.out.println("Спокойной ночи");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+    public static void main(String[] args) {
+        Jukebox song = new Jukebox();
+        int songNumber = 1;
+        song.music(songNumber);
+        songNumber = 2;
+        song.music(songNumber);
+        songNumber = 3;
+        song.music(songNumber);
+    }
+}
