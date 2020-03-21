a0dbc9c7eab42601cccedc1a27213aeaef9e0072
sousnein <souswork@yandex.ru>
2020-03-21 18:43:40 +0300

    Рефакторинг - Площадь треугольника.[#226631]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -28,8 +28,7 @@ public class Triangle {
     }
 
     static boolean exist(double a, double c, double b) {
-        boolean g=true;
-        if (a<b+c&&b < a + c&&c<a+b ){
+        if (a < b + c && b < a + c && c < a + b){
             return true;
         }
         return false;
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
