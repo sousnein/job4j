19c0451da974618ab747a5b97f5b075d971f32a9
sousnein <souswork@yandex.ru>
2020-03-24 16:07:00 +0300

    Конструктор по умолчанию.[#226645]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Animal.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+public class Animal {
+    public Animal() {
+        super();
+        System.out.println("load animal");
+    }
+    public static void main(String[]args) {
+        Tiger tiger = new Tiger();
+    }
+}
