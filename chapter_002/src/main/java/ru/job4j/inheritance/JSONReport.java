09fe385db0e09a39f1f1e452f59788a066d6a804
sousnein <souswork@yandex.ru>
2020-03-27 14:52:53 +0300

    Переопределение[#226647]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+
+    public String generate(String name, String body) {
+        return "{\n" +
+
+                "\nname : " + name + "," +
+                "\nbody : " + body +
+                "\n\n}";
+    }
+}
