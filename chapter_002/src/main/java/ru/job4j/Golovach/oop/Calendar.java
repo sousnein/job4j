f31b1edadb6c431d37a93240894e148912959ee8
sousnein <souswork@yandex.ru>
2020-05-13 09:54:37 +0300

    calendarReady

--- chapter_002/src/main/java/ru/job4j/Golovach/oop/Calendar.java
+++ chapter_002/src/main/java/ru/job4j/Golovach/oop/Calendar.java
@@ -14,5 +14,20 @@ public class Calendar {
         System.out.println("Mon Tue Wed Thu Fri Sat Sun");
         for (int i = 1; i < value; i++)
             System.out.print(" ");
+        while(date.getMonthValue()==month){
+            System.out.printf("%3d",date.getDayOfMonth());
+            if(date.getDayOfMonth()==today){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+            date=date.plusDays(1);
+            if (date.getDayOfWeek().getValue()==1){
+                System.out.println();
+            }
+        }
+        if(date.getDayOfWeek() .getValue()!= 1)
+        System.out.println();
     }
 }
